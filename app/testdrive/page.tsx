d4284d1042228f5f9ea2f3a26fda878bba4d38b5
seni0rDEV <132949016+seni0rDEV@users.noreply.github.com>
2026-03-10 10:24:14 +0100

    10:23AM 3/10/2026

---
+++ app/testdrive/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const page = () => {
+  return (
+    // <div className="w-full h-screen flex items-center justify-center">
+    //   <button className="bg-w">testdrive buitton</button>
+    // </div>
+    <h1 className='bg-h'>testdrive page</h1>
+  );
+};
+
+export default page;
