fe95f67ad7feaf4b7229a7c5e6ea9ee17944ec67
seni0rDEV <132949016+seni0rDEV@users.noreply.github.com>
2026-03-18 13:34:55 +0100

    login screen

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,7 +1,66 @@
 import React from "react";
-
+import Form from "next/form"
+import Link from "next/link";
 const page = () => {
-  return <div>Login</div>;
+  return (
+    <div className="flex flex-col justify-center h-fit w-full gap-2">
+      <div className="header w-full flex flex-col justify-center items-center">
+        <h1 className="text-[1.5rem] font-semibold">Welcome back!</h1>
+        <p className="text-[#000000] opacity-70">
+          Please login to access your account.
+        </p>
+      </div>
+      <Form
+        action="/api/auth/register"
+        className="flex flex-col gap-6 w-full h-full justify-center items-center pt-8"
+      >
+        <div className="flex flex-col w-full h-full gap-8">
+          {/* ----Email---- */}
+          <div className="input flex flex-col justify-center items-stretch  w-full gap-2">
+            <label htmlFor="email" className="text-sm text-[#414143]">
+              Your Email <span className="text-[#FF0000]">*</span>
+            </label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="ex. email@johndoe.com"
+              className="w-full h-10 rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-primary bg-[#FFFFFF] text-sm"
+              required
+            />
+          </div>
+          {/* -------Password------- */}
+          <div className="input flex flex-col justify-centerr items-stretch  w-full gap-2">
+            <label htmlFor="password" className="text-sm text-[#414143]">
+              Password <span className="text-[#FF0000]">*</span>
+            </label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              placeholder="Create a strong password"
+              className="w-full h-10 rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-primary bg-[#FFFFFF] text-sm"
+              pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+              title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters"
+              required
+            />
+          </div>
+        </div>
+        {/* <badge /> */}
+        <button className="cms-bg-primary-dark-1 text-white cms-text-sm  w-full py-2 cursor-pointer active:scale-90 rounded-lg">
+          Submit
+        </button>
+        <div className="flex items-center gap-2">
+          <p className="text-[#000000] opacity-70 ">
+            Don&apos;t have an account yet?
+          </p>
+          <Link href="/register" className="text-[#026451]">
+            Register
+          </Link>
+        </div>
+      </Form>
+    </div>
+  );
 };
 
 export default page;
